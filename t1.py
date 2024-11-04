cc463f197588769afd6b03eb8e030636ab59e228
Chineme1 <64276840+Chineme1@users.noreply.github.com>
2024-11-04 12:21:38 -0800

    Update t1.py

--- t1.py
+++ t1.py
@@ -1,11 +1,10 @@
 from flask import Flask
-import os
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def hello():
-    return "Hello from Render!"
+    return "Hello"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
+    app.run(host="0.0.0.0", port=5000)
